29eea647436e80bf022b58511c17aa3a23f913b3
Sarwagya <sarwagyasingh69@gmail.com>
2024-07-08 23:29:52 +0200

    Initial Commit : Added Base Version

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { tcp } from "@libp2p/tcp";
 import { noise } from "@chainsafe/libp2p-noise";
 import { mplex } from "@libp2p/mplex";
 import { createFromJSON, createEd25519PeerId } from "@libp2p/peer-id-factory";
+import { peerIdFromString } from "@libp2p/peer-id";
 import { config } from "dotenv";
 import cron from "node-cron";
 import fs from "fs";
@@ -11,15 +12,28 @@ import fetch from "isomorphic-fetch";
 config();
 
 const peerIdJson = fs.existsSync(process.cwd() + "/peer-id.json")
-  ? JSON.parse(fs.readFileSync(process.cwd() + "/peer-id.json"))
+  ? // Remove the quotes from the JSON string
+    fs
+      .readFileSync(process.cwd() + "/peer-id.json")
+      .toString()
+      .replace(/"/g, "")
   : null;
 
 const main = async () => {
   let peerId;
   if (!peerIdJson) {
     peerId = await createEd25519PeerId();
+
+    fs.writeFileSync(
+      process.cwd() + "/peer-id.json",
+      JSON.stringify({
+        id: peerId.toBytes(),
+        privateKey: peerId.privateKey,
+        publicKey: peerId.publicKey,
+      })
+    );
   } else {
-    peerId = await createFromJSON(peerIdJson);
+    peerId = createFromJSON(peerIdJson);
   }
 
   const node = await createLibp2p({
@@ -43,6 +57,7 @@ const main = async () => {
   cron
     .schedule("*/60 * * * * *", async () => {
       const peers = await node.peerStore.all();
+      console.log(`Pinging ${peers.length} peers`);
       for (const peer of peers) {
         try {
           await node.ping(peer);
@@ -53,6 +68,40 @@ const main = async () => {
       }
     })
     .start();
+
+  // Handle incoming connections
+  node.addEventListener("peer:connect", ({ peerId }) => {
+    console.log(`Connected to ${peerId.toB58String()}`);
+  });
+
+  // Handle incoming connections
+  node.addEventListener("peer:disconnect", ({ peerId }) => {
+    console.log(`Disconnected from ${peerId.toB58String()}`);
+  });
+
+  // Handle messages
+  node.handle("/ping/1.0.0", async ({ stream }) => {
+    console.log("Received ping");
+    stream.write({ type: "PONG" });
+  });
+
+  // Handle messages
+  node.handle("/pong/1.0.0", async ({ stream }) => {
+    console.log("Received pong");
+  });
+
+  // Handle messages
+
+  node.handle("/file/1.0.0", async ({ stream }) => {
+    console.log("Received file");
+    stream.write({ type: "FILE" });
+  });
+
+  // Handle messages
+  node.handle("/file/1.0.0", async ({ stream }) => {
+    console.log("Received file");
+    stream.write({ type: "FILE" });
+  });
 };
 
 // Ping bootnode every 30 seconds
